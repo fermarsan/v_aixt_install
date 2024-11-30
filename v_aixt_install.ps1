cf0c707507c6b67c51a2821bb06363f35ce27b26
fermarsan <fmartinezsanta@gmail.com>
2024-11-30 03:06:17 -0500

    updated by downloading from official repositories

--- v_aixt_install.ps1
+++ v_aixt_install.ps1
@@ -2,38 +2,61 @@ Set-PSDebug -Trace 0    #echo off
 
 # this is a batch file to automate the installing of V and Aixt transpilers on Windows
 
-# Download arduino-cli.zip from official page
+# Install git
+if (-not (Get-Command git)) {  # if 'git' doesn't exist
+    Write-Output "`nInstalling git..."
+    winget install --id Git.Git -e --source winget
+}
+
+# Download v.zip from the official repository
 if (-not (Test-Path -Path "..\v.zip")) {  # if the .zip file doesn't exist
-    Write-Output "`nDownloading v.zip from official repository..."
+    Write-Output "`nDownloading v.zip from the official repository..."
     Invoke-WebRequest "https://github.com/vlang/v/archive/refs/heads/master.zip" -OutFile "..\v.zip"
 }
 
 # Unzip v.zip to C:\v
-if (-not (Test-Path -Path 'C:\v')) {    # if the folder doesn't exist
-    Write-Output 'Unzipping v_windows.zip to C:\v...'
-    Expand-Archive -Path '.\v_windows.zip' -DestinationPath 'C:\'
+if (-not (Test-Path -Path "C:\v")) {    # if the folder doesn't exist
+    Write-Output "Unzipping v.zip to C:\v..."
+    Expand-Archive -Path "..\v.zip" -DestinationPath "C:\v"
 } 
 
-# Create v symlink
-if (-not (Get-Command v)) { # if the command doesn't exist
-    Write-Output "Creating V symlink..."
-    $env:PATH += ";C:\v\"
-    [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
-}
+# Compile V
+cd c:\v
+& ".\make.bat"
 
+# Create v symlink
+# if (-not (Get-Command v)) { # if the command doesn't exist
+#     Write-Output "Creating V symlink..."
+#     $env:PATH += ";C:\v\"
+#     [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
+# }
+.\v.exe symlink
+
+
+# Download aixt.zip from the official repository
+if (-not (Test-Path -Path "..\aixt.zip")) {  # if the .zip file doesn't exist
+    Write-Output "`nDownloading aixt.zip from the official repository..."
+    Invoke-WebRequest "https://github.com/fermarsan/aixt/archive/refs/heads/main.zip" -OutFile "..\aixt.zip"
+}
 
 # Unzip aixt.zip to C:\aixt
 if (-not (Test-Path -Path 'C:\aixt')) { # if the folder doesn't exist
-    Write-Output "Unzipping aixt_windows.zip to C:\aixt..."
-    Expand-Archive -Path '.\aixt_windows.zip' -DestinationPath 'C:\'
+    Write-Output "Unzipping aixt.zip to C:\aixt..."
+    Expand-Archive -Path '..\aixt.zip' -DestinationPath 'C:\aixt'
 }
 
-# Create aixt symlink
-if (-not (Get-Command aixt)) {  # if the command doesn't exist
-    Write-Output "Creating Aixt symlink..."
-    $env:PATH += ";C:\aixt\"
-    [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
-}
+
+# Compile Aixt
+cd c:\aixt
+& ".\make.bat"
+
+# # Create aixt symlink
+# if (-not (Get-Command aixt)) {  # if the command doesn't exist
+#     Write-Output "Creating Aixt symlink..."
+#     $env:PATH += ";C:\aixt\"
+#     [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
+# }
+.\aixt.exe symlink
 
 # Restore the execution policy
 Set-ExecutionPolicy Restricted
